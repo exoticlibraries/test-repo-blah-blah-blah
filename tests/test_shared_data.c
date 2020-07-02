d53627fa6a5a383e43f53c1cd73b96dc8d2a2b4c
Adewale Azeez <azeezadewale98@gmail.com>
2020-07-02 22:25:59 +0100

    Update test_shared_data.c

--- tests/test_shared_data.c
+++ tests/test_shared_data.c
@@ -4,20 +4,20 @@
 
 #include <exotic/cester.h>
 
-CESTER_BEFORE_ALL(test_instance,
-    test_instance->arg = (void*) "Hello World";
-)
+//CESTER_BEFORE_ALL(test_instance,
+//    test_instance->arg = (void*) "Hello World";
+//)
 
-CESTER_BEFORE_EACH(test_instance, name, index,
-    printf("Isolated Str %s\n", (char*)test_instance->arg);
-    printf("Isolated %d\n", superTestInstance.isolate_tests);
-)
+//CESTER_BEFORE_EACH(test_instance, name, index,
+//    printf("Isolated Str %s\n", (char*)test_instance->arg);
+//    printf("Isolated %d\n", superTestInstance.isolate_tests);
+//)
 
 CESTER_TEST(check_number_equals, test_instance,
-    cester_assert_true(cester_string_equals((char*)test_instance->arg, "Hello World"));
-    cester_assert_not_null(test_instance->arg);
-    cester_assert_equal(test_instance->arg, test_instance->arg);
-    cester_assert_not_equal(NULL, test_instance->arg);
+    //cester_assert_true(cester_string_equals((char*)test_instance->arg, "Hello World"));
+    //cester_assert_not_null(test_instance->arg);
+    //cester_assert_equal(test_instance->arg, test_instance->arg);
+    //cester_assert_not_equal(NULL, test_instance->arg);
 )
     
 CESTER_TEST(test_one, inst,
