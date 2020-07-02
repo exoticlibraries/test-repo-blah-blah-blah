edd7c5b058cce9911b99bb74f8eb4e4e3663753c
Adewale Azeez <azeezadewale98@gmail.com>
2020-07-02 22:35:45 +0100

    Update test_shared_data.c

--- tests/test_shared_data.c
+++ tests/test_shared_data.c
@@ -14,10 +14,10 @@ CESTER_BEFORE_ALL(test_instance,
 //)
 
 CESTER_TEST(check_number_equals, test_instance,
-    //cester_assert_true(cester_string_equals((char*)test_instance->arg, "Hello World"));
-    //cester_assert_not_null(test_instance->arg);
-    //cester_assert_equal(test_instance->arg, test_instance->arg);
-    //cester_assert_not_equal(NULL, test_instance->arg);
+    cester_assert_true(cester_string_equals((char*)test_instance->arg, "Hello World"));
+    cester_assert_not_null(test_instance->arg);
+    cester_assert_equal(test_instance->arg, test_instance->arg);
+    cester_assert_not_equal(NULL, test_instance->arg);
 )
     
 CESTER_TEST(test_one, inst,
