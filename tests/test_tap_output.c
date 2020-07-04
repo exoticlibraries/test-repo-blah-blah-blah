c44b54ee60c27fe4ff31fc0edae5e5927c89732c
Adewale Azeez <azeezadewale98@gmail.com>
2020-07-04 23:15:25 +0100

    Update test_tap_output.c

--- tests/test_tap_output.c
+++ tests/test_tap_output.c
@@ -16,15 +16,15 @@ typedef struct a_struct {
 
 CESTER_BEFORE_ALL(test_instance,
     AStruct* arg_value = (AStruct*) malloc(sizeof(AStruct*));
-		  printf("P1=%p\n", (void *)arg_value);
     arg_value->index = 20;
     arg_value->value = "A string";
     test_instance->arg = arg_value;
+  	printf("P2=%p, I1=%d, S1=%s\n", (void *)arg_value, arg_value->index, arg_value->value);
 )
 
 CESTER_TEST(modify_test_instance, test_instance,
     AStruct* arg_value = ((AStruct*)test_instance->arg);
-	    printf("P2=%p\n", (void *)arg_value);
+	    printf("P2=%p, I1=%d, S1=%s\n", (void *)arg_value, arg_value->index, arg_value->value);
     cester_assert_equal(arg_value->index, 20);
 	    arg_value->index++;
     cester_assert_equal(arg_value->value, "A string");
@@ -36,7 +36,6 @@ CESTER_TODO_TEST(after_we_write_the_array_struct, test_instance,
 )
 
 CESTER_TEST(test_direct_variables, test_instance,
-	    printf("P2=%p\n", ((AStruct*)test_instance->arg));
     cester_assert_int_eq(((AStruct*)test_instance->arg)->index, 21);
     cester_assert_str_not_equal(((AStruct*)test_instance->arg)->value, "A string");
     cester_assert_str_equal(((AStruct*)test_instance->arg)->value, "Unknown");
