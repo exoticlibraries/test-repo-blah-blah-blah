2cfe72a72f32b1c32debc368b227bf6aa4e2ca7a
Adewale Azeez <azeezadewale98@gmail.com>
2020-07-05 05:34:15 +0100

    Update test_shared_data.c

--- tests/test_shared_data.c
+++ tests/test_shared_data.c
@@ -4,20 +4,29 @@
 
 #include <exotic/cester.h>
 
+CESTER_BODY(
+typedef struct param_arg {
+    char *value;
+} Param;    
+)
+
 CESTER_BEFORE_ALL(test_instance,
-    test_instance->arg = (char *) "Hello World";
+                  Param* param = (Param*) malloc(sizeof(Param));
+                  param->value = (char *) "Hello World";
+    test_instance->arg = param;
 )
 
 CESTER_TEST(check_number_equals, test_instance,
-    cester_assert_true(cester_string_equals((char*)test_instance->arg, "Hello World"));
-    cester_assert_not_null(test_instance->arg);
-    cester_assert_equal(test_instance->arg, test_instance->arg);
-    cester_assert_not_equal(NULL, test_instance->arg);
-            printf("The Data 1 %s\n", ((char*)(test_instance->arg)));
+            Param* param = (Param*) test_instance->arg;
+    cester_assert_true(cester_string_equals(param->value, "Hello World"));
+    cester_assert_not_null(param->value);
+    cester_assert_equal(param->value, param->value);
+    cester_assert_not_equal(NULL, param->value);
+            printf("The Data 1 %s\n", ((char*)(param->value)));
 )
 
 CESTER_TEST(validate_shared_arg, test_instance,
-            printf("The Data %s\n", test_instance->arg);
+            //printf("The Data %s\n", test_instance->arg);
             //printf("The Data %p %s %d\n", test_instance->arg, test_instance->arg, cester_string_equals((char*)test_instance->arg, "Hello World"));
             //printf("The Data %p %s %d\n", test_instance->arg, test_instance->arg, cester_string_equals((char*)test_instance->arg, "World"));
             //printf("The Data %p %s %d\n", test_instance->arg, test_instance->arg, cester_string_equals((char*)test_instance->arg, "Hello"));
