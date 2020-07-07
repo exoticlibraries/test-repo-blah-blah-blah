14298e12e11df90756135042b6d0a4e5d189894b
Adewale Azeez <azeezadewale98@gmail.com>
2020-07-07 02:15:02 +0100

    Update test_shared_data.c

--- tests/test_shared_data.c
+++ tests/test_shared_data.c
@@ -1,41 +1,68 @@
-/*!gcc -ansi -pedantic-errors {0} -IC -I../include/ -o out; ./out */
+/*!gcc {0} -I. -I../include/ -o out; ./out --cester-output=tap */
 
-/* this test shares data between tests */
-
+#ifndef __BASE_FILE__
+#define CESTER_NO_MAIN
+#endif
 #include <exotic/cester.h>
 
 CESTER_BODY(
-typedef struct param_arg {
-    char *value;
-} Param;    
-    
-char *value = "Hello World";
+
+typedef struct a_struct {
+    int index ;
+    char* value;
+} AStruct;
+
 )
 
 CESTER_BEFORE_ALL(test_instance,
-                 
+    AStruct* arg_value = (AStruct*) malloc(sizeof(AStruct*));
+    arg_value->index = 20;
+    arg_value->value = "A string";
+    test_instance->arg = arg_value;
 )
 
-CESTER_TEST(check_number_equals, test_instance,
-    cester_assert_true(cester_string_equals(value, "Hello World"));
-    cester_assert_not_null(value);
-    cester_assert_equal(value, value);
-    cester_assert_not_equal(NULL, value);
-            printf("The Data 1 %s\n", value);
+CESTER_TEST(modify_test_instance, test_instance,
+    AStruct* arg_value = ((AStruct*)test_instance->arg);
+    cester_assert_equal(arg_value->index++, 20);
+    cester_assert_equal(arg_value->value, "A string");
+    arg_value->value = "Unknown";
 )
 
-CESTER_TEST(validate_shared_arg, test_instance,
-            //printf("The Data %s\n", test_instance->arg);
-            //printf("The Data %p %s %d\n", test_instance->arg, test_instance->arg, cester_string_equals((char*)test_instance->arg, "Hello World"));
-            //printf("The Data %p %s %d\n", test_instance->arg, test_instance->arg, cester_string_equals((char*)test_instance->arg, "World"));
-            //printf("The Data %p %s %d\n", test_instance->arg, test_instance->arg, cester_string_equals((char*)test_instance->arg, "Hello"));
-    //cester_assert_true(cester_string_equals((char*)test_instance->arg, "Hello World"));
-    //cester_assert_false(cester_string_equals((char*)test_instance->arg, "World"));
-    //cester_assert_false(cester_string_equals((char*)test_instance->arg, "Hello"));
+CESTER_TODO_TEST(after_we_write_the_array_struct, test_instance,
+    cester_assert_nothing();
+)
+
+CESTER_TEST(test_direct_variables, test_instance,
+    cester_assert_int_eq(((AStruct*)test_instance->arg)->index, 21);
+    cester_assert_str_not_equal(((AStruct*)test_instance->arg)->value, "A string");
+    cester_assert_str_equal(((AStruct*)test_instance->arg)->value, "Unknown");
+)
+
+CESTER_TODO_TEST(write_test_after_implementation, test_instance,
+    cester_assert_nothing();
+)
+
+CESTER_SKIP_TEST(skip_this_test_it_breaks, test_instance,
+    cester_assert_true('a' == 'b');
 )
 
 CESTER_OPTIONS(
+    CESTER_OUTPUT_TAP();
     CESTER_VERBOSE();
     CESTER_MINIMAL();
     CESTER_NO_ISOLATION();
 )
+
+#ifndef __BASE_FILE__
+CESTER_BODY(
+int main(int argc, char** argv) {
+    CESTER_REGISTER_BEFORE_ALL();
+    CESTER_REGISTER_SKIP_TEST(skip_this_test_it_breaks);
+	CESTER_REGISTER_TEST(modify_test_instance);
+	CESTER_REGISTER_TEST(test_direct_variables);
+    CESTER_REGISTER_TODO_TEST(write_test_after_implementation);
+    CESTER_REGISTER_TODO_TEST(after_we_write_the_array_struct);
+	return CESTER_RUN_ALL_TESTS(argc, argv);
+}
+)
+#endif
